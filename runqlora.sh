b409ea3ca50114ed54d18d5ebc6366adc61ff656
PrasannSinghal <prasannsinghal.dev@gmail.com>
2023-06-12 16:24:14 +0000

    Qlora stuff + alpaca farm stuff

--- runqlora.sh
+++ runqlora.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=1,2,3
-python -u qlora/qlora.py --model_name_or_path "/home/prasann/Projects/tfr-decoding/llama/llama" --dataset="/home/prasann/Projects/tfr-decoding/output/llamaftunetrain.jsonl"
+python -u qlora/qlora.py --model_name_or_path "/home/prasann/Projects/tfr-decoding//llama/llama" --dataset="/home/prasann/Projects/tfr-decoding//output/llamaftunetrain.jsonl"
